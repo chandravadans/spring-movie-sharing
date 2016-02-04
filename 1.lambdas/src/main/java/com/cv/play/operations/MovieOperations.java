796509e293d25a4ff1b1a177574b5d87c0ed593a
Chandravadan <s.chandravadan@gmail.com>
2016-02-04 08:28:59 +0530

    Formatted

--- 1.lambdas/src/main/java/com/cv/play/operations/MovieOperations.java
+++ 1.lambdas/src/main/java/com/cv/play/operations/MovieOperations.java
@@ -8,15 +8,17 @@ import com.cv.play.model.Movie;
 
 public class MovieOperations {
 	// Sorting and filtering operations performed on lists of movies
-	public static void sortByName(List<Movie> movies){
-		Collections.sort(movies, 
-				(m1,m2) -> { return m1.getName().compareTo(m2.getName());});
-		
+	public static void sortByName(List<Movie> movies) {
+		Collections.sort(movies, (m1, m2) -> {
+			return m1.getName().compareTo(m2.getName());
+		});
+
 	}
-	public static List<Movie> filterByActor(List<Movie> movies, String actorName){
+
+	public static List<Movie> filterByActor(List<Movie> movies, String actorName) {
 		List<Movie> results = movies.stream()
-							  .filter(m -> (m.getLeadActor().compareTo(actorName))==0)
-							  .collect(Collectors.toList());
+				.filter(m -> (m.getLeadActor().compareTo(actorName)) == 0)
+				.collect(Collectors.toList());
 		return results;
 	}
 }
--- 1.lambdas/src/test/java/com/cv/play/test/AllTests.java
+++ 1.lambdas/src/test/java/com/cv/play/test/AllTests.java
@@ -15,43 +15,45 @@ import com.cv.play.model.Movie;
 import com.cv.play.operations.MovieOperations;
 
 public class AllTests {
-	
+
 	@Test
-	public void sortsByMovieName(){
+	public void sortsByMovieName() {
 		PodamFactoryImpl randomBeanFactory = new PodamFactoryImpl();
 		List<Movie> movies = new ArrayList<Movie>();
-		for(int i=0;i<5;i++){
+		for (int i = 0; i < 5; i++) {
 			Movie m = randomBeanFactory.manufacturePojo(Movie.class);
-			System.out.println("Random movie # "+i+": "+m.getName());
+			System.out.println("Random movie # " + i + ": " + m.getName());
 			movies.add(m);
 		}
 		MovieOperations.sortByName(movies);
-		for(int i=0;i<movies.size()-1;i++){
-			if(movies.get(i).getName().compareTo(movies.get(i+1).getName())>0)
+		for (int i = 0; i < movies.size() - 1; i++) {
+			if (movies.get(i).getName().compareTo(movies.get(i + 1).getName()) > 0)
 				fail("Not sorted!");
 		}
 	}
-	
+
 	@Test
-	public void filtersByActorName(){
+	public void filtersByActorName() {
 		String actorName = "Ranvir";
 		PodamFactoryImpl randomBeanFactory = new PodamFactoryImpl();
 		List<Movie> movies = new ArrayList<Movie>();
-		for(int i=0;i<5;i++){
+		for (int i = 0; i < 5; i++) {
 			Movie m = randomBeanFactory.manufacturePojo(Movie.class);
-			if(i%2 == 0)
+			if (i % 2 == 0)
 				m.setLeadActor("Ranbir");
 			else
 				m.setLeadActor("Ranvir");
-			System.out.println("Random movie # "+i+": "+m.getLeadActor());
+			System.out.println("Random movie # " + i + ": " + m.getLeadActor());
 			movies.add(m);
 		}
-		
-		List<Movie> filteredList = MovieOperations.filterByActor(movies, "Ranvir");
-		for(Movie m : filteredList){
-			if(actorName.compareTo(m.getLeadActor())!=0)
-				fail(m.getName()+" doesn't cast "+actorName+" in the lead role!");
+
+		List<Movie> filteredList = MovieOperations.filterByActor(movies,
+				"Ranvir");
+		for (Movie m : filteredList) {
+			if (actorName.compareTo(m.getLeadActor()) != 0)
+				fail(m.getName() + " doesn't cast " + actorName
+						+ " in the lead role!");
 		}
-		
+
 	}
 }
