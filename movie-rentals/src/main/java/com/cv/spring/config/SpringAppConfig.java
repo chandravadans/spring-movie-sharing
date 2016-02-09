5f19c34aa9207473ea50264591ee75cdbcbb6cfc
Chandravadan <cv@morningschool.com>
2016-02-09 12:24:16 +0530

    Removed unnecessary imports in Config

--- movie-rentals/src/main/java/com/cv/spring/config/SpringAppConfig.java
+++ movie-rentals/src/main/java/com/cv/spring/config/SpringAppConfig.java
@@ -1,6 +1,5 @@
 package com.cv.spring.config;
 
-import org.springframework.beans.factory.config.PropertyResourceConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +10,7 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 public class SpringAppConfig {
 
 	@Bean
-	public static PropertySourcesPlaceholderConfigurer initConfigurator(){
+	public static PropertySourcesPlaceholderConfigurer initConfigurator(){ 
 		return new PropertySourcesPlaceholderConfigurer();
 	}
 
